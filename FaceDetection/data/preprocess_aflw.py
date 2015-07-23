f9d816726cd3a7550cb3fe315c1345b1086a47f5
dan <warmspringwinds@gmail.com>
2015-07-23 16:37:42 +0200

    one more change

--- FaceDetection/data/preprocess_aflw.py
+++ FaceDetection/data/preprocess_aflw.py
@@ -14,8 +14,8 @@ image_path = os.path.join(BASE_DIR, 'data/aflw/data/flickr/')
 image_save_path = os.path.join(BASE_DIR, 'data/processed_images/')
 
 
-def extract_faces(output_size):
-    file_name = "{}_{}_faces".format(*(output_size))
+def extract_faces(output_size, amount_of_faces_to_extract):
+    file_name = "{}_{}_faces_aflw".format(*(output_size))
     new_dir_name = os.path.join(image_save_path, file_name)
 
     if not os.path.exists(new_dir_name):
@@ -31,46 +31,57 @@ def extract_faces(output_size):
         query = "SELECT FaceRect.face_id , FaceRect.x, FaceRect.y, FaceRect.w, FaceRect.h, FaceImages.file_id, " \
                 "FaceImages.filepath, FaceImages.width, FaceImages.height from FaceRect  inner join Faces on " \
                 "FaceRect.face_id = Faces.face_id inner join FaceImages on Faces.file_id = FaceImages.file_id"
-        #
+
         res = c.execute(query)
 
-    faces = []
-
     face_save_count = 0
+    #faces = res.fetchall()
 
-    for i in range(no_rows):
+    for i in xrange(no_rows):
+
         face = res.fetchone()
 
-        col = face[1]
-        row = face[2]
-        width = face[3]
-        height = face[4]
+        print face
 
-        image_file_path = face[6]
-        path = image_path + image_file_path
+        try:
 
-        image = io.imread(path)
+            col = face[1]
+            row = face[2]
+            width = face[3]
+            height = face[4]
 
-        if len(image.shape) < 3:
+            image_file_path = face[6]
+            path = image_path + image_file_path
+
+            image = io.imread(path)
+
+            if len(image.shape) < 3:
+                continue
+
+            if image.shape[2] == 1:
+                continue
+
+            image_max_row = image.shape[0] - 1
+            image_max_col = image.shape[1] - 1
+
+            if row < 0 or col < 0 or row > image_max_row or col > image_max_col:
+                continue
+
+            im = image[row: row + height, col: col + width]
+            im = resize(im, output_size)
+
+            save_file_string = os.path.join(new_dir_name, "{0:06d}".format(face_save_count) + ".jpg")
+
+            io.imsave(save_file_string, im)
+
+        except:
             continue
 
-        if image.shape[2] == 1:
-            continue
-
-        image_max_row = image.shape[0] - 1
-        image_max_col = image.shape[1] - 1
-
-        if row < 0 or col < 0 or row > image_max_row or col > image_max_col:
-            continue
-
-        im = image[row: row + height, col: col + width]
-        im = resize(im, output_size)
-
-        save_file_string = os.path.join(new_dir_name, "{0:06d}".format(face_save_count) + ".jpg")
-
-        io.imsave(save_file_string, im)
         face_save_count += 1
 
+        if face_save_count == amount_of_faces_to_extract:
+            break
+
 if __name__ == '__main__':
-    extract_faces((13, 13))
-    extract_faces((25, 25))
+    extract_faces((28, 28), 25000)
+    #extract_faces((25, 25))
